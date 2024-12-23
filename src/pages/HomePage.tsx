0db8e7f951c2a07a2ed2cfc47aa73648d8bcfa79
fanhe <hee.fan@outlook.com>
2024-12-23 23:25:25 +0800

    feat: add react-router support, split home page and welcome page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div className="text-center p-4">
+      <h1 className="text-4xl font-bold">这是主页噢！</h1>
+      <p className="mt-2 text-lg">我是Ane, Now a Front-End Developer</p>
+    </div>
+  );
+}
---
+++ src/pages/WelcomePage.tsx
@@ -0,0 +1,8 @@
+export default function WelcomePage() {
+  return (
+    <div className="text-center p-4">
+      <h1 className="text-4xl font-bold">欢迎来到我的博客！</h1>
+      <p className="mt-2 text-lg">这是一个基于 Vite + React + UnoCSS 的个人博客项目。</p>
+    </div>
+  );
+}
