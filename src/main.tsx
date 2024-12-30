4904b1c1ed69a3987d7da58146ca47e9872a0373
fanhe <hee.fan@outlook.com>
2024-12-31 00:09:45 +0800

    feat: reset browser default styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import 'virtual:uno.css'
+import '@unocss/reset/tailwind.css'
 
 import App from './App.tsx'
 
